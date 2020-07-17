f1ab2279e903ff6221e4010ce51af99122a77664
FZxiao <42691214+fzxiao233@users.noreply.github.com>
2020-07-17 17:37:00 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '111',
+  ver: '112',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -12,10 +12,16 @@ jsproxy_config({
 
   // 节点配置
   node_map: {
-    'mysite': {
-      label: '当前站点',
+    'Cloudflare': {
+      label: 'Cloudflare',
       lines: {
         'https://ao3.fzxiao.workers.dev': 1,
+      }
+    },
+    'Sakura-JP': {
+    label: 'Sakura-JP',
+      lines: {
+        'http://133.167.92.105:8912/': 1,
       }
     }
     }
@@ -24,13 +30,13 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'mysite',
+  node_default: 'Sakura-JP',
   // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
    * 加速节点
    */
-  node_acc: 'mysite',
+  node_acc: 'Sakura-JP',
 
   /**
    * 静态资源 CDN 地址
